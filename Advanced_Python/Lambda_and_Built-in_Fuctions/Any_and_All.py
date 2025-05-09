217040004fe9ec4ede843c49e64c54402c2dc15b
Emre Enes Yenen <emreenesyenenn@gmail.com>
2025-05-09 23:44:24 +0300

    python

--- Advanced_Python/Lambda_and_Built-in_Fuctions/Any_and_All.py
+++ Advanced_Python/Lambda_and_Built-in_Fuctions/Any_and_All.py
@@ -18,4 +18,18 @@ sayilar=[1,3,5,7,6,5,52,0]
 sonuc=all([bool(sayi)for sayi in sayilar])
 sonuc=any([bool(sayi)for sayi in sayilar])
 
+
+
+
+sonuc = all([sayi%2==0  for sayi in sayilar])
+sonuc = any([sayi%2==0  for sayi in sayilar])
+
+
+users=["emre","enes","ahmet"]
+sonuc = all([user[0]=="a" for user in users])
+sonuc = any([user[0]=="a" for user in users])
+
+
+
+
 print(sonuc)
