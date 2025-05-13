c7c63792773502bd4014f7c6fb6e5ba3294a43e4
Emre Enes Yenen <emreenesyenenn@gmail.com>
2025-05-13 23:15:14 +0300

    ClassConstructors

---
+++ Advanced_Python/OOP/ClassConstructors.py
@@ -0,0 +1,14 @@
+class CarItem:
+    def __init__(self,name,price,quantity):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+
+
+
+item1=CarItem("telefon",50000,2)
+item2=CarItem("Bilgisayar",70000,1)
+item3=CarItem("Kitap",200,2)
+
+
